0ae1da8768974118476e09a0b5eea1dffc4488af
cpk1 <cpk1@pacbell.net>
2017-12-19 17:14:16 -0800

    save work on thresholds report

--- samples/bollingerBands.R
+++ samples/bollingerBands.R
@@ -14,31 +14,30 @@ library(forecast)
 #  movingAvg calculates a moving average for the input vector x
 #           using a window of size "n" (default 5).
 #
-movingAvg <- function(x,n=5){filter(x,rep(1/n,n), sides=1)}
+movingAvg <- function(x,n=5){filter(x,rep(1/n,n), sides=2)}
 
 #########################
 #
-#  forecastAlertThresholds executes a simple model to predict the behavior of a
+#  bollingerThresholds executes a simple model to predict the behavior of a
 #       given metric over the next 24 hours.
 #
-#       Given two time series of samples for a given metric on two days,
-#       this function averages the two series, and smoothes the result with a
-#       moving average (default window is 8 samples). The Standard Deviation
+#       Given a time series of samples for a given metric on an averaged day,
+#       this function smoothes the day with a moving average (default window 
+#       is 5 samples, two sided). The Standard Deviation of the detrended data
 #       is then added and subtracted to the moving average to create the 
 #       upper and lower bounds for a Bollinger Band. 
 #
-forecastAlertThresholds <- function(day1, day2, ma_window=8) {
+bollingerThresholds <- function(avgDay, ma_window=5) {
     # align the time ranges so that we can average both series
-    day_2 <- ts(day2,start=start(day1),end=end(day1),frequency = frequency(day1))
-    mavgDay <- na.omit(movingAvg((day1+day_2)/2, n=ma_window))
+    mavgDay <- na.omit(movingAvg(avgDay, n=ma_window))
 
     # ndiffs tells us how many differences we need to take in order to remove
     # the trend component from our data
-    detrended_mavg <- diff(mavgDay,differences=ndiffs(mavgDay))
+    detrended_avg <- diff(avgDay,differences=ndiffs(avgDay))
 
     # create upper & lower bounds by biasing the smoothed average up and down
     # relative to the standard deviation of the stationarized smoothed data.
-    sdDay <- sd(detrended_mavg)
+    sdDay <- sd(detrended_avg)
     #print(sdDay)
     upperBound <- mavgDay + 2*sdDay
     lowerBound <- pmax(mavgDay - 2*sdDay,0)     # dont allow series to go negative
@@ -55,18 +54,31 @@ cacheName <- "EmsQueueTotalsByServer"
 filterColumn <- "URL"
 filterValue <- "tcp://VMIRIS1023:7222"
 columns <- "time_stamp;pendingMessageCount"  # set analysis column
+rtvQuery <- "simdata2_rtvquery"
 #
 #################
 #
-# To forecast the next day's trend, get same day for the last two weeks.
-dayMinus6 <- getCacheHistory(dataserver,cacheName, "simdata2_rtvquery", fcol=filterColumn,fval=filterValue,dayOffset=6,ndays=1,cols=columns)
-dayMinus13 <- getCacheHistory(dataserver,cacheName, "simdata2_rtvquery", fcol=filterColumn,fval=filterValue,dayOffset=13,ndays=1,cols=columns)
+#    estimateTypicalDay
 
-# forecast the alert thresholds for the next day
-thresholds <- forecastAlertThresholds(dayMinus6, dayMinus13)
+# To forecast the next day's trend, get same day of the week for the last two weeks.
+day1 <- getCacheHistory(dataserver,cacheName, rtvQuery, fcol=filterColumn,fval=filterValue,dayOffset=6,secOffset=2700,ndays=1,cols=columns)
+day2 <- getCacheHistory(dataserver,cacheName, rtvQuery, fcol=filterColumn,fval=filterValue,dayOffset=13,secOffset=2700,ndays=1,cols=columns)
+
+# set a common time range (tomorrow) for both series
+day1 <- ts(day1, start=start(day1)+c(7,0), end=end(day1)+c(7,0), frequency=frequency(day1))
+day2 <- ts(day2, start=start(day1), end=end(day1), frequency=frequency(day1))
+
+# estimate as average of given days
+nextDayEstimate <- (day1+day2)/2 #+ runif(length(day1), 10, 5000)
+
+# forecast the alert thresholds for tomorrow
+thresholds <- bollingerThresholds(nextDayEstimate, ma_window=7)
 
 # plot the forecasted thresholds
-thresholds <- ts(thresholds,start=start(thresholds)+7,end=end(thresholds)+7,frequency=frequency(thresholds))
-plot(thresholds[,1], ylab="pending messages", main="Dynamic Thresholds for Next Day", ylim=c(min(thresholds[,2]),max(thresholds[,1])))
+plot(thresholds[,1], ylab="pending messages", main="Expected Threshold Bounds for Day Ahead", ylim=c(min(thresholds[,2]),max(thresholds[,1])))
 lines(thresholds[,2])
 lines(thresholds[,3],col=3,lty=3)
+
+#################
+#
+
--- samples/sl_utils.R
+++ samples/sl_utils.R
@@ -13,27 +13,41 @@ getCacheCurrent <- function(rtvServer, cache, package="emsample_rtvquery", fmt="
 	read.delim(url)     # execute REST query; returns an R dataframe
 }
 
-
-#  getCacheHistory queries the RTView REST interface for history data.
+###############################################################################
 #
-#       By default, this function pulls 24 hours of pendingMessageCount
-#       data for a given day from the EmsQueueTotalsByServer cache, which  
-#       is indexed by URL of the Tibco EMS server; as seen in the function 
-#       declaration, the query filter parms (fcol, fval) and cache can be 
-#       overridden to retrieve data from other caches.
+#  getCacheHistory - query the RTView REST interface for history data.
+#
+#       As commonly used, this function pulls "n" days of data from an RTView cache,
+#       starting from an offset in days. The use of an offset makes it easy to
+#       retrieve data for modeling without the need to do date arithmetic to
+#       calculate start and end times for the desired time series. The data is
+#       coerced into an R time-series, with the time scaled in terms of days and
+#       sample offset into the day. Hence, if the series is plotted, the x axis 
+#       will represent time in days since 1970-01-01.
+#
+#       Note: when RTView time series data is returned, the time span should
+#       be chosen to ensure that the sample spacing is constant throughout 
+#       the series, as assumed by the R "ts" class. The function computes an
+#       average delta-t for the series, and uses this to estimate the time for 
+#       the last sample in order to return a valid ts object, so the final sample
+#       time may vary slightly from the actual history segment.
+#       (TBD) If the first and last delta-time of the series differ by more than
+#       one second, the time series should be aggregated so that all samples are
+#       spaced the last delta-t apart.
+#
+#       The first column returned by the query must be a timestamp.
 #
 #   Examples:
 #       # retrieve history data for the EMS server indexed in the cache by 
 #       # URL "tcp://192.168.1.116:7222" on the sixth data prior to "today"
-#       h6 <- getHistory("192.168.1.101","EmsQueueTotalsByServer", "emsmon_rtvquery2", "tcp://192.168.1.116:7222",dayOffset=6)
+#       h6 <- getHistory("192.168.1.101", fval="tcp://192.168.1.116:7222",dayOffset=6)
 #
 getCacheHistory <- function(rtvServer, cache="EmsQueueTotalsByServer", 
-						package="emsample_rtvquery", fval, fcol="URL", 
-						dayOffset=0, ndays=1, tz_offset=8,
-						cols="time_stamp",
-						fmt="text", tr=86400) {
+						rtvquery="emsample_rtvquery", fval, fcol="URL", 
+						dayOffset=6, secOffset=0, sides=2, ndays=1, tz_offset=8,
+						cols="time_stamp", fmt="text", tr=86400) {
     # set up the base URL for the REST query
-    base_url <- sprintf("http://%s/%s/cache/%s/history?fmt=%s&cols=%s",rtvServer,package,cache,fmt,cols)
+    base_url <- sprintf("http://%s/%s/cache/%s/history?fmt=%s&cols=%s",rtvServer,rtvquery,cache,fmt,cols)
 	# create a column filter to retrieve rows for a specified index value
     emsFilter <- sprintf("fcol=%s&fval=%s",fcol,fval)
     
@@ -44,27 +58,55 @@ getCacheHistory <- function(rtvServer, cache="EmsQueueTotalsByServer",
         timeFilter <- sprintf("tr=%s",tr)
         #timeFilter <- sprintf("tr=%s&te=%s000",tr,te)
         print("set tr only")
-        this.frequency <- .1
     } else {
-        tb <- (unclass(Sys.Date()) - dayOffset)*86400 + tz_offset*3600
-        trr <- tr * ndays
+        tb <- (unclass(Sys.Date()) - dayOffset)*86400 - secOffset + tz_offset*3600
+        trr <- tr * ndays + sides*secOffset
         timeFilter <- sprintf("tr=%s&tb=%s000",trr,tb)
-        this.frequency <- 96
     }
 
     url <- URLencode(paste(base_url,emsFilter,timeFilter,"sqlex=true",sep="&"))
     #print(paste(">>>fetching URL: ",url))        # debug
     ret <- read.delim(url, sep="\t")     # execute REST query; returns an R dataframe
-    ret$time_stamp <- as.POSIXct(ret$time_stamp,"%b %d, %Y %I:%M:%S %p",tz="")
-    delta_t <- difftime(ret$time_stamp[2],ret$time_stamp[1],units="secs")
-    if (this.frequency == 96) {
-        ts(ret[,-1], start=as.Date(ret$time_stamp[1]), end=as.Date(ret$time_stamp[length(ret$time_stamp)]), frequency=this.frequency, deltat=delta_t)
+    num_rows <- nrow(ret)
+    if (num_rows != 0) {
+        ret$time_stamp <- as.POSIXct(ret$time_stamp,"%b %d, %Y %I:%M:%S %p",tz="GMT")
+        delta <- as.numeric(difftime(ret$time_stamp[2],ret$time_stamp[1],units="secs"))
+        end_delta <- as.numeric(difftime(ret$time_stamp[num_rows],ret$time_stamp[num_rows-1],units="secs"))
+        if (floor(delta/end_delta) > 1) {
+            #aggregate samples at lower sample spacing than last pair of samples
+            print("*** getCacheHistory: need to aggregate part of return data")
+        }
+        delta_t <- floor(as.numeric(difftime(ret$time_stamp[num_rows],ret$time_stamp[1],units="secs"))/(num_rows-1))
+        this.frequency <- floor(86400/delta_t)  # calculate number of samples per day
+        #print(delta_t)
+        #print(this.frequency)
+        ts(ret[,-1], start=getTsTime(ret$time_stamp[1],delta_t), end=getTsTime(ret$time_stamp[1],delta_t,num_rows),
+            frequency=this.frequency, deltat=delta_t)
     }
     else {
-        endtime <- as.numeric(ret$time_stamp[1]) + delta_t * length(ret$time_stamp)
-        ts(ret[,-1], start=as.numeric(ret$time_stamp[1]), end=endtime, frequency=this.frequency, deltat=delta_t)
+        print("*** getCacheHistory: RTView query returned no data")
     }
 }
+
+###############################################################################
+#
+#  getTsTime  - calculate suitable start and end parameters for an R time-series.
+#               The given date is converted into a two element list "c(ndays,index)",
+#               where ndays is the number of days since 1970-01-01, and index is
+#               the sample index (from 1..frequency, where frequency is the expected
+#               number of samples per day spaced at intervals of deltat).
+#
+#   posixctDate - a date-time object of class POSIXct.
+#   deltat      - the interval between data samples in seconds for the given time series.
+#
+
+getTsTime <- function(posixctDate, deltat, numSamples=1) {
+    secSince1970 <- as.numeric(posixctDate + (numSamples-1)*deltat)
+    dayPart <- floor(secSince1970/86400)
+    sampleOffset <- floor((secSince1970 - dayPart*86400)/deltat) + 1
+    c(dayPart, sampleOffset)
+}
+
 
 
 # get mean and confidence interval
